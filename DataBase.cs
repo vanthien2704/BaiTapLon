cbc34db1a204c66810c64ceb7dc938ab33f41500
ACER <ACER@VANTHIEN>
2023-11-13 14:33:11 +0700

    Add project files.

---
+++ DataBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace BaiTapLon
+{
+    class DataBase
+    {
+        public static SqlConnection MyCon = new SqlConnection(@"Data Source=localhost;Initial Catalog=QLBANHANG;Persist Security Info=True;User ID=admin;Password=admin");
+
+    }
+}
