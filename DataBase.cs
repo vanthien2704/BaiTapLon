d2f58d2c13b27b25a07852438433e545655bf9be
VanThien <hoc727675@gmail.com>
2023-11-16 10:03:50 +0700

    update SqlConnection from App.config to DataBase

--- DataBase.cs
+++ DataBase.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace BaiTapLon
 {
     class DataBase
     {
-        public static SqlConnection SqlConnection = new SqlConnection($"Data Source=localhost;Initial Catalog=QLBANHANG;Persist Security Info=True;User ID=admin;Password=admin");
+        public static SqlConnection SqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["BaiTapLon.Properties.Settings.QLBANHANGConnectionString"].ConnectionString);
     }
 }
